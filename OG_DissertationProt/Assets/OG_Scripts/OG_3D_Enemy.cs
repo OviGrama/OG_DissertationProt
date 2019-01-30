d00e2b9adbbd4038e03aff9a6bd8b7fdc846e355
OviGrama <44976044+OviGrama@users.noreply.github.com>
2019-01-30 15:05:52 +0000

    Muzzle Flash, Auto Fire, Shoot Sound, Impact Effect Default
    Muzzle Flash, Auto Fire, Shoot Sound, Impact Effect Default

---
+++ OG_DissertationProt/Assets/OG_Scripts/OG_3D_Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OG_3D_Enemy : MonoBehaviour
+{
+
+    public float fl_health = 100;
+
+    public void TakeDamage(float amount)
+    {
+        fl_health -= amount;
+        if(fl_health <= 0f)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+
+
+}
