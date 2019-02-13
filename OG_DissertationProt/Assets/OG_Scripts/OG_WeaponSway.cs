1231611066fd7ef8a14e86b38b4b0d11a74a6583
OviGrama <44976044+OviGrama@users.noreply.github.com>
2019-02-13 17:59:32 +0000

    Animation, Reload, UI, ADS !
    Animation (Idle, Fire, Reload)
    Reload and ADS functions
    UI (Ammo - current / bullets left)

---
+++ OG_DissertationProt/Assets/OG_Scripts/OG_WeaponSway.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OG_WeaponSway : MonoBehaviour
+{
+    public float fl_swayAmount;
+    public float fl_maxAmount;
+    public float fl_smoothAmount;
+
+    private Vector3 initialPos;
+
+
+    void Start()
+    {
+        initialPos = transform.localPosition;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementX = -Input.GetAxis("Mouse X") * fl_swayAmount;
+        float movementY = -Input.GetAxis("Mouse Y") * fl_swayAmount;
+
+        movementX = Mathf.Clamp(movementX, -fl_maxAmount, fl_maxAmount);
+        movementY = Mathf.Clamp(movementY, -fl_maxAmount, fl_maxAmount);
+
+        Vector3 finalPos = new Vector3(movementX, movementY, 0);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, finalPos + initialPos, Time.deltaTime * fl_smoothAmount);
+    }
+}
